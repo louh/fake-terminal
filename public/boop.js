b5d55a83ba50332e4341ce0b40a53a14b74faebf
Lou Huang <lou@louhuang.com>
2017-04-15 22:55:44 -0400

    make this work with require()

--- public/boop.js
+++ public/boop.js
@@ -2,75 +2,96 @@
 /**
 * Instance of Terminal
 */
-var commands = {}
-var state = {}
-
-commands.help = function() {
-  var output = "<div>" +
-    "<p><strong>Fake Terminal</strong> is a thing that looks like a terminal but it does not do anything for real. Ideal for </p>" +
-    "<p>easter eggs, boss screens, April Fool's jokes, and more. Currently this does not do a whole lot but</p>" +
-    "<p>with your help we can make this the best fake terminal in the world.</p>" +
-    "<p>&nbsp;</p>" +
-    "<p>Join the project or remix this on Glitch: <a href=\"https://glitch.com/edit/#!/fake-terminal\" target=\"_blank\">https://glitch.com/edit/#!/fake-terminal</a></p>" +
-    "<p>Or on GitHub: <a href=\"https://github.com/louh/fake-terminal\" target=\"_blank\">https://github.com/louh/fake-terminal</a></p>" +
-    "<p>&nbsp;</p>" +
-    "<p>Here are the currently available commands:</p>" +
-    "<ul>" +
-    "<li><strong>help</strong> - display this help.</li>" +
-    "<li><strong>echo &lt;string&gt</strong> - write arguments to the standard output</li>" +
-    "<li><strong>exit</strong> - kill terminal process</li>" +
-    "<li><strong>su &lt;username&gt</strong> - substitute user identity</li>" +
-    "</ul></div>"
-  return output
-}
-
-commands.echo = function (args) {
-  args.shift()
-  return args.join(' ')
-}
-
-/**
- * Doom emulator
- */
-commands.iddqd = function () {
-  if (!state.iddqd) {
-    state.iddqd = true
-    return 'Degreelessness mode on'
+(function (root, factory) {
+  if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but only CommonJS-like
+    // environments that support module.exports, like Node.
+    module.exports = factory(require('./terminal.js'), 'node')
   } else {
-    state.iddqd = false
-    return 'Degreelessness mode off'
+    // Browser globals (root is window)
+    root.initTerminal = factory(root.Terminal, 'browser')
   }
-}
+}(this, function (Terminal, mode) {
+  var commands = {}
+  var state = {}
 
-/**
- * Zork emulator
- */
-commands.look = function (args) {
-  if (args.length <= 1) {
-    return 'You are standing in an open field west of a white house, with a boarded front door. There is a small mailbox here.<br><br>'
+  commands.help = function() {
+    var output = "<div>" +
+      "<p><strong>Fake Terminal</strong> is a thing that looks like a terminal but it does not do anything for real. Ideal for </p>" +
+      "<p>easter eggs, boss screens, April Fool's jokes, and more. Currently this does not do a whole lot but</p>" +
+      "<p>with your help we can make this the best fake terminal in the world.</p>" +
+      "<p>&nbsp;</p>" +
+      "<p>Join the project or remix this on Glitch: <a href=\"https://glitch.com/edit/#!/fake-terminal\" target=\"_blank\">https://glitch.com/edit/#!/fake-terminal</a></p>" +
+      "<p>Or on GitHub: <a href=\"https://github.com/louh/fake-terminal\" target=\"_blank\">https://github.com/louh/fake-terminal</a></p>" +
+      "<p>&nbsp;</p>" +
+      "<p>Here are the currently available commands:</p>" +
+      "<ul>" +
+      "<li><strong>help</strong> - display this help.</li>" +
+      "<li><strong>echo &lt;string&gt</strong> - write arguments to the standard output</li>" +
+      "<li><strong>exit</strong> - kill terminal process</li>" +
+      "<li><strong>su &lt;username&gt</strong> - substitute user identity</li>" +
+      "</ul></div>"
+    return output
+  }
+
+  commands.echo = function (args) {
+    args.shift()
+    return args.join(' ')
+  }
+
+  /**
+   * Doom emulator
+   */
+  commands.iddqd = function () {
+    if (!state.iddqd) {
+      state.iddqd = true
+      return 'Degreelessness mode on'
+    } else {
+      state.iddqd = false
+      return 'Degreelessness mode off'
+    }
+  }
+
+  /**
+   * Zork emulator
+   */
+  commands.look = function (args) {
+    if (args.length <= 1) {
+      return 'You are standing in an open field west of a white house, with a boarded front door. There is a small mailbox here.<br><br>'
+    } else {
+      return 'I don\'t know the word "' + args[1] + '".<br><br>'
+    }
+  }
+
+  commands.su = function (args) {
+    if (args.length > 1) {
+      Terminal.user = args[1]
+    }
+    return ''
+  }
+
+  commands.exit = function (args) {
+    Terminal.exit()
+    console.log('[Process completed]')
+  }
+
+  function initTerminal() {
+    console.log('Terminal access granted.')
+
+    Terminal.init(document.body, {
+      commands: commands,
+      prompt: '\\u@\\H $ ',
+      intro: '<p>Welcome to Fake Terminal. Type \'help\' to get started.</p><p>&nbsp;</p>'
+    })
+  }
+
+  // Return a value to define the module export.
+  if (mode === 'node') {
+    return {
+      initTerminal: initTerminal,
+      Terminal: Terminal
+    }
   } else {
-    return 'I don\'t know the word "' + args[1] + '".<br><br>'
+    return initTerminal
   }
-}
-
-commands.su = function (args) {
-  if (args.length > 1) {
-    Terminal.user = args[1]
-  }
-  return ''
-}
-
-commands.exit = function (args) {
-  Terminal.exit()
-  console.log('[Process completed]')
-}
-
-function initTerminal() {
-  console.log('Terminal access granted.')
-
-  Terminal.init(document.body, {
-    commands: commands,
-    prompt: '\\u@\\H $ ',
-    intro: '<p>Welcome to Fake Terminal. Type \'help\' to get started.</p><p>&nbsp;</p>'
-  })
-}
+}))
--- public/terminal.js
+++ public/terminal.js
@@ -1,205 +1,219 @@
 /**
  * pseudo-terminal adapted from https://github.com/avgp/terminal.js
  */
-var Terminal = (function () {
-  var history = (localStorage.getItem('history') ? localStorage.getItem('history').split(',') : [])
-  var historyIndex = history.length
-  var self = {}
-
-  var KEY_UP = 38
-  var KEY_DOWN = 40
-  var KEY_TAB = 9
-
-  // Auxiliary functions
-
-  var resetPrompt = function (terminal, prompt) {
-    var newPrompt = prompt.parentNode.cloneNode(true)
-
-    prompt.setAttribute('contenteditable', false)
-    newPrompt.querySelector('.terminal-prompt').textContent = getPrompt(self.options.prompt)
-    terminal.appendChild(newPrompt)
-    newPrompt.querySelector('.terminal-input').innerHTML = ' '
-    newPrompt.querySelector('.terminal-input').focus()
+(function (root, factory) {
+  if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but only CommonJS-like
+    // environments that support module.exports, like Node.
+    module.exports = factory()
+  } else {
+    // Browser globals (root is window)
+    root.Terminal = factory()
   }
-  
-  var tryCommand = function (terminal, cmd, args) {
-    if (cmd in self.commands) {
-      runCommand(terminal, cmd, args)
-    } else {
-      commandNotFound(terminal, cmd)
-    }
-  }
+}(this, function () {
+  var Terminal = (function () {
+    var history = (localStorage.getItem('history') ? localStorage.getItem('history').split(',') : [])
+    var historyIndex = history.length
+    var self = {}
 
-  var runCommand = function (terminal, cmd, args) {
-    terminal.innerHTML += (self.commands[cmd](args))
-  }
+    var KEY_UP = 38
+    var KEY_DOWN = 40
+    var KEY_TAB = 9
 
-  var commandNotFound = function (terminal, cmd) {
-    terminal.innerHTML += cmd + ': command not found'
-  }
+    // Auxiliary functions
 
-  var updateHistory = function (cmd) {
-    history.push(cmd)
-    localStorage.setItem('history', history)
-    historyIndex = history.length
-  }
+    var resetPrompt = function (terminal, prompt) {
+      var newPrompt = prompt.parentNode.cloneNode(true)
 
-  var browseHistory = function (prompt, direction) {
-    var changedPrompt = false
-
-    if (direction === KEY_UP && historyIndex > 0) {
-      prompt.textContent = history[--historyIndex]
-      changedPrompt = true
-    } else if (direction === KEY_DOWN) {
-      if (historyIndex < history.length) ++historyIndex
-      if (historyIndex < history.length) prompt.textContent = history[historyIndex]
-      else prompt.textContent = " "
-      changedPrompt = true
+      prompt.setAttribute('contenteditable', false)
+      newPrompt.querySelector('.terminal-prompt').textContent = getPrompt(self.options.prompt)
+      terminal.appendChild(newPrompt)
+      newPrompt.querySelector('.terminal-input').innerHTML = ' '
+      newPrompt.querySelector('.terminal-input').focus()
     }
 
-    if (changedPrompt) {
-      var range = document.createRange()
-      var sel = window.getSelection()
-      range.setStart(prompt.childNodes[0], prompt.textContent.length)
-      range.collapse(true)
-      sel.removeAllRanges()
-      sel.addRange(range)
-    }
-  }
-
-  var autoCompleteInput = function (input) {
-    var cmds = self.commands
-    var re = new RegExp("^" + input, "ig")
-    var suggestions = []
-    for (var cmd in cmds) {
-      if (cmds.hasOwnProperty(cmd) && cmd.match(re)) {
-        suggestions.push(cmd)
+    var tryCommand = function (terminal, cmd, args) {
+      if (cmd in self.commands) {
+        runCommand(terminal, cmd, args)
+      } else {
+        commandNotFound(terminal, cmd)
       }
     }
-    return suggestions
-  }
-  
-  var getPrompt = function (str, opts) {
-    return str
-      .replace('\\u', self.user)
-      .replace('\\H', window.location.hostname)
-      .replace('\\h', window.location.hostname.split('.')[0])
-  }
-  
-  var createElements = function (opts) {
-    var container = document.createElement('div')
-    container.classList.add('terminal')
-    
-    var fragment = document.createDocumentFragment()
-    var elem = document.createElement('div')
-    elem.className = 'terminal-output'
-    elem.setAttribute('spellcheck', false)
-    
-    var intro = document.createElement('div')
-    intro.innerHTML = opts.intro
-    elem.appendChild(intro)
-    
-    var line = document.createElement('p')
-    var prompt = document.createElement('span')
-    prompt.className = 'terminal-prompt'
-    prompt.innerHTML = getPrompt(opts.prompt, opts)
-    line.appendChild(prompt)
-    var input = document.createElement('span')
-    input.className = 'terminal-input'
-    input.setAttribute('contenteditable', true)
-    line.appendChild(input)
-    elem.appendChild(line)
 
-    fragment.appendChild(elem)
-    container.appendChild(fragment)
-
-    self.output = elem
-    return container
-  }
-  
-  var mountTerminalElement = function (mount, el) {
-    if (mount instanceof window.HTMLElement) {
-      mount.appendChild(el)
-    } else if (typeof mount === 'string') {
-      var existing = document.getElementById(mount)
-      if (!existing) return
-      existing.appendChild(el)
+    var runCommand = function (terminal, cmd, args) {
+      terminal.innerHTML += (self.commands[cmd](args))
     }
 
-    return
-  }
+    var commandNotFound = function (terminal, cmd) {
+      terminal.innerHTML += cmd + ': command not found'
+    }
 
-  // Terminal functions
+    var updateHistory = function (cmd) {
+      history.push(cmd)
+      localStorage.setItem('history', history)
+      historyIndex = history.length
+    }
 
-  self.init = function (containerId, opts) {
-    self.options = opts
-    self.user = opts.user || 'root'
-    self.commands = opts.commands
-    self.container = createElements(opts)
-    
-    mountTerminalElement(containerId, self.container)
-    
-    self.output.addEventListener('keydown', function (event) {
-      if (event.keyCode === KEY_TAB) {
-        var prompt = event.target
-        var suggestions = autoCompleteInput(prompt.textContent.replace(/\s+/g, ''))
+    var browseHistory = function (prompt, direction) {
+      var changedPrompt = false
 
-        if (suggestions.length === 1) {
-          prompt.textContent = suggestions[0]
-          var range = document.createRange()
-          var sel = window.getSelection()
-          range.setStart(prompt.childNodes[0], suggestions[0].length)
-          range.collapse(true)
-          sel.removeAllRanges()
-          sel.addRange(range)
+      if (direction === KEY_UP && historyIndex > 0) {
+        prompt.textContent = history[--historyIndex]
+        changedPrompt = true
+      } else if (direction === KEY_DOWN) {
+        if (historyIndex < history.length) ++historyIndex
+        if (historyIndex < history.length) prompt.textContent = history[historyIndex]
+        else prompt.textContent = " "
+        changedPrompt = true
+      }
+
+      if (changedPrompt) {
+        var range = document.createRange()
+        var sel = window.getSelection()
+        range.setStart(prompt.childNodes[0], prompt.textContent.length)
+        range.collapse(true)
+        sel.removeAllRanges()
+        sel.addRange(range)
+      }
+    }
+
+    var autoCompleteInput = function (input) {
+      var cmds = self.commands
+      var re = new RegExp("^" + input, "ig")
+      var suggestions = []
+      for (var cmd in cmds) {
+        if (cmds.hasOwnProperty(cmd) && cmd.match(re)) {
+          suggestions.push(cmd)
         }
-
-        event.preventDefault(true)
-        return false
       }
-    })
+      return suggestions
+    }
 
-    self.output.addEventListener('keyup', function (event) {
-      if (historyIndex < 0) return
-      browseHistory(event.target, event.keyCode)
-    })
+    var getPrompt = function (str, opts) {
+      return str
+        .replace('\\u', self.user)
+        .replace('\\H', window.location.hostname)
+        .replace('\\h', window.location.hostname.split('.')[0])
+    }
 
-    self.output.addEventListener('keypress', function (event) {
-      var prompt = event.target
-      if (event.keyCode !== 13) return false
+    var createElements = function (opts) {
+      var container = document.createElement('div')
+      container.classList.add('terminal')
 
-      updateHistory(prompt.textContent)
+      var fragment = document.createDocumentFragment()
+      var elem = document.createElement('div')
+      elem.className = 'terminal-output'
+      elem.setAttribute('spellcheck', false)
 
-      var input = prompt.textContent.trim().split(' ')
-      if (input[0]) tryCommand(self.output, input[0], input)
+      var intro = document.createElement('div')
+      intro.innerHTML = opts.intro
+      elem.appendChild(intro)
 
-      resetPrompt(self.output, prompt)
-      event.preventDefault()
-    })
-    
-    /**
-     * Clicking anywhere on terminal should put cursor on the command line
-     * Do not focus on command line if action creates a text selection range
-     */
-    self.output.addEventListener('click', function (event) {
-      // Sometimes there is more than one editable input, force it to be the last one
-      // TODO: fix the bug where there is more than one contenteditable element
-      var temp = self.output.querySelectorAll('.terminal-input[contenteditable=true]')
-      var el = temp[temp.length - 1]
-      var selection = window.getSelection()
-      if (selection.isCollapsed === true && selection.rangeCount <= 1 && el) {
-        el.focus()
+      var line = document.createElement('p')
+      var prompt = document.createElement('span')
+      prompt.className = 'terminal-prompt'
+      prompt.innerHTML = getPrompt(opts.prompt, opts)
+      line.appendChild(prompt)
+      var input = document.createElement('span')
+      input.className = 'terminal-input'
+      input.setAttribute('contenteditable', true)
+      line.appendChild(input)
+      elem.appendChild(line)
+
+      fragment.appendChild(elem)
+      container.appendChild(fragment)
+
+      self.output = elem
+      return container
+    }
+
+    var mountTerminalElement = function (mount, el) {
+      if (mount instanceof window.HTMLElement) {
+        mount.appendChild(el)
+      } else if (typeof mount === 'string') {
+        var existing = document.getElementById(mount)
+        if (!existing) return
+        existing.appendChild(el)
       }
-    }, false)
-    
-    self.output.querySelector('.terminal-input').focus()
+
+      return
+    }
+
+    // Terminal functions
+
+    self.init = function (containerId, opts) {
+      self.options = opts
+      self.user = opts.user || 'root'
+      self.commands = opts.commands
+      self.container = createElements(opts)
+
+      mountTerminalElement(containerId, self.container)
+
+      self.output.addEventListener('keydown', function (event) {
+        if (event.keyCode === KEY_TAB) {
+          var prompt = event.target
+          var suggestions = autoCompleteInput(prompt.textContent.replace(/\s+/g, ''))
+
+          if (suggestions.length === 1) {
+            prompt.textContent = suggestions[0]
+            var range = document.createRange()
+            var sel = window.getSelection()
+            range.setStart(prompt.childNodes[0], suggestions[0].length)
+            range.collapse(true)
+            sel.removeAllRanges()
+            sel.addRange(range)
+          }
+
+          event.preventDefault(true)
+          return false
+        }
+      })
+
+      self.output.addEventListener('keyup', function (event) {
+        if (historyIndex < 0) return
+        browseHistory(event.target, event.keyCode)
+      })
+
+      self.output.addEventListener('keypress', function (event) {
+        var prompt = event.target
+        if (event.keyCode !== 13) return false
+
+        updateHistory(prompt.textContent)
+
+        var input = prompt.textContent.trim().split(' ')
+        if (input[0]) tryCommand(self.output, input[0], input)
+
+        resetPrompt(self.output, prompt)
+        event.preventDefault()
+      })
+
+      /**
+       * Clicking anywhere on terminal should put cursor on the command line
+       * Do not focus on command line if action creates a text selection range
+       */
+      self.output.addEventListener('click', function (event) {
+        // Sometimes there is more than one editable input, force it to be the last one
+        // TODO: fix the bug where there is more than one contenteditable element
+        var temp = self.output.querySelectorAll('.terminal-input[contenteditable=true]')
+        var el = temp[temp.length - 1]
+        var selection = window.getSelection()
+        if (selection.isCollapsed === true && selection.rangeCount <= 1 && el) {
+          el.focus()
+        }
+      }, false)
+
+      self.output.querySelector('.terminal-input').focus()
+      return self
+    }
+
+    self.exit = function () {
+      var removeThis = self.container.parentNode.removeChild(self.container)
+      removeThis = null
+    }
+
     return self
-  }
-  
-  self.exit = function () {
-    var removeThis = self.container.parentNode.removeChild(self.container)
-    removeThis = null
-  }
+  })()
 
-  return self
-})()
+  // Return a value to define the module export.
+  return Terminal
+}))
